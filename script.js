2b68d23d81a0284e9c7a126ec688beb26f9cef59
jordidbmf <jordi@zamplex.com.br>
2023-10-29 16:45:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,130 @@
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco ')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button ')
+const startPauseBt = document.querySelector('#start-pause')
+
+//Desafio
+const iconePauseBt = document.querySelector('.app__card-primary-butto-icon') 
+//
+
+const tempoNaTela = document.querySelector('#timer')
+
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+
+let tempoDecorridoEmSecundos = 1500
+let intervaloId = null
+
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
+
+//Desafio
+const audioStart = new Audio('/sons/play.wav')
+const audioPause = new Audio('/sons/pause.mp3')
+const audioEnd = new Audio('/sons/beep.mp3')
+// 
+
+focoBt.addEventListener('click', () => {
+    /*html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', '/imagens/foco.png')*/
+    tempoDecorridoEmSecundos = 1500
+    alterarContexto('foco')
+    focoBt.classList.add('active')
+})
+
+curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSecundos = 300
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
+})
+
+longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSecundos = 900
+    alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
+})
+
+function alterarContexto(contexto) {
+    mostrarTempo()
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            <strong class="app__title-strong">Hora de voltar à superfície.
+            Faça uma pausa longa.</strong>
+            `
+            break;
+        default:
+            break;
+    }
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSecundos <= 0){
+        audioEnd.play()        
+        alert('Tempo finalizado!')
+        zerar()
+        return
+    }
+    tempoDecorridoEmSecundos -= 1
+    //console.log('Temporizador: ' + tempoDecorridoEmSecundos)
+    mostrarTempo()
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        audioStart.play()        
+        zerar()
+        return
+    }
+    audioPause.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = "Pausar"
+    iconePauseBt.setAttribute('src', '/imagens/pause.png')
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    iniciarOuPausarBt.textContent = "Começar"
+    iconePauseBt.setAttribute('src', '/imagens/play_arrow.png')
+    intervaloId = null
+}
+
+function mostrarTempo() {
+    //const tempo = tempoDecorridoEmSecundos
+    const tempo = new Date(tempoDecorridoEmSecundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
